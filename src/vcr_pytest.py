aafde49ff0a6a26960bbf74a0068779a7cb6a293
Tyler Turner <tzturner@illinois.edu>
2023-01-17 14:47:22 -0600

    TODO

--- src/vcr_pytest.py
+++ src/vcr_pytest.py
@@ -33,28 +33,30 @@ VCR_RECORD = "VCR_RECORD" in os.environ
 #    conn.logger.setLevel(logging.INFO)
 #    return conn
 
-@pytest.fixture
-def cassette(request) -> vcr.cassette.Cassette:
-    my_vcr = vcr.VCR(
-        cassette_library_dir='cassettes',
-        record_mode='once' if VCR_RECORD else 'none',
-        # TODO: Uncomment with remove_creds from shared repo
-        # before_record_request=remove_creds,
-        filter_headers=[('Authorization', 'Bearer FAKE_TOKEN')],
-        match_on=['uri', 'method'],
-    )
-####### Pseudo-code
-    serializer = CleanYAMLSerializer()
-    # TODO: Apply all cleaners from @cleaner decorator
-    # serializer.register_cleaner(imported_fun1)
-    # serializer.register_cleaner(builtin_fun2)
-    my_vcr.register_serializer("cleanyaml", serializer)
-######
-    # my_vcr.register_serializer("cleanyaml", CleanYAMLSerializer)
+# TODO: Discuss: can we generalize this or should we just remove it?
 
-    with my_vcr.use_cassette(f'{request.function.__name__}.yaml',
-                             serializer="cleanyaml") as tape:
-        yield tape
-        if my_vcr.record_mode == 'none':  # Tests only valid when not recording
-            assert tape.all_played, \
-                f"Only played back {len(tape.responses)} responses"
+# @pytest.fixture
+# def cassette(request) -> vcr.cassette.Cassette:
+#     my_vcr = vcr.VCR(
+#         cassette_library_dir='cassettes',
+#         record_mode='once' if VCR_RECORD else 'none',
+#         # TODO: Uncomment with remove_creds from shared repo
+#         # before_record_request=remove_creds,
+#         filter_headers=[('Authorization', 'Bearer FAKE_TOKEN')],
+#         match_on=['uri', 'method'],
+#     )
+# ####### Pseudo-code
+#     serializer = CleanYAMLSerializer()
+#     # TODO: Apply all cleaners from @cleaner decorator
+#     # serializer.register_cleaner(imported_fun1)
+#     # serializer.register_cleaner(builtin_fun2)
+#     my_vcr.register_serializer("cleanyaml", serializer)
+# ######
+#     # my_vcr.register_serializer("cleanyaml", CleanYAMLSerializer)
+
+#     with my_vcr.use_cassette(f'{request.function.__name__}.yaml',
+#                              serializer="cleanyaml") as tape:
+#         yield tape
+#         if my_vcr.record_mode == 'none':  # Tests only valid when not recording
+#             assert tape.all_played, \
+#                 f"Only played back {len(tape.responses)} responses"
