f15e7ee577df9faef64598bfba4ec5b1a465f2f1
Edward Delaporte <edward@delaporte.us>
2023-06-15 19:33:07 +0000

    New test for clean_env_response

--- tests/test_env_string.py
+++ tests/test_env_string.py
@@ -14,67 +14,76 @@ def get_body_string(body):
         }
     }
 
-
-@pytest.mark.parametrize('body,expected', [
+CLEAN_TEST_PARAMS = [
     ('CLEAN ME', 'CLEANED'),
     ('CLEAN THIS TOO', 'CLEANED'),
     ('CLEAN ME and CLEAN THIS TOO', 'CLEANED and CLEANED'),
     ('NOTHING TO CLEAN', 'NOTHING TO CLEAN'),
+    (b'CLEAN ME', b'CLEAN ME'),  # Current expected behavior - may change
     (b'DEADBEEF', b'DEADBEEF'),  # Regression for #20
-])
+]
+
+
+def get_body(body, request=""):
+    return {
+        'response': {
+            'body': body
+        },
+        'request': {
+            'body': request
+        },
+    }
+
+
+@pytest.mark.parametrize('body,expected', CLEAN_TEST_PARAMS)
 def test_clean_env_string_key(monkeypatch, body, expected):
     '''Test cases where there is a key named 'string' in 'body'.
     '''
     monkeypatch.setenv('CLEAN_STRINGS', ALWAYS_CLEAN)
-
     interaction = get_body_string(body)
-    clean_env_strings(None, interaction['response'])
-    result = interaction['response']['body']['string'] 
 
+    clean_env_strings(None, interaction['response'])
+
+    result = interaction['response']['body']['string'] 
     assert result == expected
     assert interaction == get_body_string(expected)
 
 
-
-def get_body(body):
-    return {
-        'response': {
-            'body': body
-        }
-    }
-
-
-
-@pytest.mark.parametrize('body,expected', [
-    ('CLEAN ME', 'CLEANED'),
-    ('CLEAN THIS TOO', 'CLEANED'),
-    ('CLEAN ME and CLEAN THIS TOO', 'CLEANED and CLEANED'),
-    ('NOTHING TO CLEAN', 'NOTHING TO CLEAN'),
-    (b'DEADBEEF', b'DEADBEEF'),
-    (b'CLEAN ME', b'CLEAN ME'),
-])
-def test_clean_env_string(monkeypatch, body, expected):
+@pytest.mark.parametrize('body,expected', CLEAN_TEST_PARAMS)
+def test_clean_env_body(monkeypatch, body, expected):
     monkeypatch.setenv('CLEAN_STRINGS', ALWAYS_CLEAN)
-
     interaction = get_body(body)
-    clean_env_strings(None, interaction['response'])
-    result = interaction['response']['body']
 
+    clean_env_strings(None, interaction['response'])
+
+    result = interaction['response']['body']
     assert result == expected
     assert interaction == get_body(expected)
 
 
-@pytest.mark.parametrize('body,expected', [
-    ('CLEAN ME', 'CLEAN ME'),
-    ('CLEAN THIS TOO', 'CLEAN THIS TOO'),
-    (b'DEADBEEF', b'DEADBEEF'),
-    (b'CLEAN ME', b'CLEAN ME'),
-])
-def test_env_unset(monkeypatch, body, expected):
+@pytest.mark.parametrize('body', 
+                         (lambda: x for x,_ in CLEAN_TEST_PARAMS))
+def test_env_unset(monkeypatch, body):
+    """Nothing happens when the ENV variable is not set."""
+    monkeypatch.setenv('CLEAN_STRINGS', ALWAYS_CLEAN)
     interaction = get_body(body)
-    clean_env_strings(None, interaction['response'])
-    result = interaction['response']['body']
 
+    clean_env_strings(None, interaction['response'])
+
+    result = interaction['response']['body']
+    assert result == body 
+    assert interaction == get_body(body)
+
+
+@pytest.mark.parametrize('body,expected', CLEAN_TEST_PARAMS)
+def test_clean_env_request(monkeypatch, body, expected):
+    """Clean requests as well as responses"""
+    monkeypatch.setenv('CLEAN_STRINGS', ALWAYS_CLEAN)
+    interaction = get_body(body="", request=body)
+
+    clean_env_strings(interaction['request'], None)
+
+    result = interaction['request']['body']
     assert result == expected
-    assert interaction == get_body(expected)
+    assert interaction == get_body(body= "", request=expected)
 
