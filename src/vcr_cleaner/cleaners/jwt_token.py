bea85a2cf4178c182f9c045cd599ca8d3d5e7b4b
Zach Carrington <zdc@illinois.edu>
2026-07-07 11:39:14 -0500

    Remove token base 64 encode

--- src/vcr_cleaner/cleaners/jwt_token.py
+++ src/vcr_cleaner/cleaners/jwt_token.py
@@ -7,11 +7,11 @@ CLEANER_SALT = 'PleaseChangeThisSaltToSomethingElse'
 CLEANER_JWT_TOKEN = {'exp': datetime.datetime(2049, 6, 25)}
 
 
-def clean_token(request: dict, response: dict):
+def clean_token(request: dict, response: dict, algorithm='HS256'):
     '''Clean a JWT token.'''
 
-    jwt_token = jwt.encode(CLEANER_JWT_TOKEN, CLEANER_SALT, algorithm='HS256')
+    jwt_token = jwt.encode(
+        CLEANER_JWT_TOKEN, CLEANER_SALT, algorithm)
     if 'Content-Encoding' in response['headers'].keys() and \
             response['headers']['Content-Encoding'] == ['gzip']:
-        token = gzip.compress(bytes(jwt_token, "ascii"))
-        response['body']['string'] = token
+        response['body']['string'] = jwt_token
